d81b206621c7569febce1a4eb0e4c63ca98984b6
Karthik73965 <karthikjidagam81@gmail.com>
2024-01-12 11:54:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Fotter from "./components/Fotter";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+   <>
+      <Navbar/>
+      <Home/>
+      <Fotter/>
+   </>
+  )
+}
