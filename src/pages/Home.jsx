3014edd58b3b4d2308f471056fbec4d5ccde2652
Karthik73965 <karthikjidagam81@gmail.com>
2024-01-17 16:09:01 +0530

    CTA

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Home/Hero'
 import Feautures from '../components/Home/Feautures'
 import UseCases from '../components/Home/UseCases'
 import Institutes from '../components/Home/Institutes'
+import CTA from '../components/Home/CTA'
 
 export default function () {
   return (
@@ -11,6 +12,7 @@ export default function () {
      <Feautures/>
      <UseCases/>
      <Institutes/>
+     <CTA/>
     </>
   )
 }
