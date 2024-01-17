b8bd22433226aa13e3d3cb22d28223de3307c20f
Karthik73965 <karthikjidagam81@gmail.com>
2024-01-17 14:45:58 +0530

    component -UseCases

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Home/Hero'
 import Feautures from '../components/Home/Feautures'
+import UseCases from '../components/Home/UseCases'
 
 export default function () {
   return (
     <>
      <Hero/>
      <Feautures/>
+     <UseCases/>
     </>
   )
 }
