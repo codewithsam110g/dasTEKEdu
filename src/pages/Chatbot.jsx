716bddc1508d17f82b3d7a3ef281c9d462582cf5
Karthik73965 <karthikjidagam81@gmail.com>
2024-01-24 16:23:56 +0530

    minor change - shifting nav bar to individual page

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -11,7 +11,7 @@ import { FaCopy } from "react-icons/fa";
 
 
 
-const PhotoUploadComponent = () => {
+const CHATBOT= () => {
     const [inputValue, setInputValue] = useState('');
     const [selectedFile, setSelectedFile] = useState(null);
     const [loading, setLoading] = useState(false);
