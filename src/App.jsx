1da2235d1ffb62a6b4d1e3be4e915c0738e9a10f
codewithsam110g <beautiful110g@gmail.com>
2024-01-25 12:02:13 +0530

    Added User Auth and changed few things like Home and Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
 import Fotter from "./components/Fotter";
-import Navbar from "./components/Navbar";
 import HOMEWORKHELP from "./pages/HomeWorkHelp";
 import Home from "./pages/Home";
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import Chatbot from "./pages/Chatbot";
 import GeneralChatbot from "./pages/GeneralChatbot";
 import Threads from "./pages/Threads";
 import IndividualThread from "./pages/IndividualThread";
+import { UserAuthContextProvider } from "./UserAuth";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-   <>
-      
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/threads" element={<Threads/>} />
-        <Route path="/HOMEWORK-HELP" element={<HOMEWORKHELP/>} />
-        <Route path="/login" element={<LogIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/chatbot" element={<Chatbot/>} />
-        <Route path="/gen-chatbot" element={<GeneralChatbot/>} />
-        <Route path='/threads/threads.id' element={<IndividualThread/>} />
-      </Routes>
-      <Fotter/>
-   </>
-  )
+    <>
+      <UserAuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/threads" element={<Threads />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <HOMEWORKHELP />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/gen-chatbot" element={<GeneralChatbot />} />
+          <Route path="/threads/threads.id" element={<IndividualThread />} />
+        </Routes>
+        <Fotter />
+      </UserAuthContextProvider>
+    </>
+  );
 }
